db91738766e854307e1f8bc345f83d3981d72d81
trix-04 <jxt_04@outlook.com>
2024-11-14 22:20:21 -0500

    Edited JS file to add function that validates first name, last name and email, as well as checks for completeness and correct format.

--- script.js
+++ script.js
@@ -12,3 +12,42 @@ for (const button of buttons) {
         }
     });
 }
+
+const form = document.getElementById('contactForm');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', function(event) {
+    clearErrors();
+    let isFormValid = true;
+
+    if (firstName.value.trim() == '') {
+        showError(firstName, "Please enter your first name.");
+        isFormValid = false;
+    } else if (!isValidName(firstName.value)) {
+        showError(firstName, "First name should not contain any numbers or special characters.");
+        isFormValid = false;
+    }
+
+    if (lastName.value.trim() == '') {
+        showError(lastName, "Please enter your last name.");
+        isFormValid = false;
+    } else if (!isValidName(lastName.value)) {
+        showError(lastName, "Last name should not contain any numbers or special characters.");
+        isFormValid = false;
+    }
+
+    if (email.value.trim() == '') {
+        showError(email, "Please enter your email.");
+        isFormValid = false;
+    } else if (!isValidEmail(email.value)) {
+        showError(email, "Please enter a valid email address.");
+        isFormValid = false;
+    }
+
+    if (!isFormValid) {
+        event.preventDefault();
+    }
+});
